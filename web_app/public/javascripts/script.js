ffdf391738f3a0bf32c812e26e82d4d17aac5338
Prashant Ghimire <prashant.ghimire@selu.edu>
2016-05-05 02:19:39 -0500

    added thumbs buttons

--- web_app/public/javascripts/script.js
+++ web_app/public/javascripts/script.js
@@ -1,40 +1,45 @@
 angular.module('expressmusic',['ui.router'])
 
-.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise("/app");
-	$stateProvider
-	.state('app',{
-		url: '/app',
-		templateUrl: 'home',
-		controller: 'HomeCtrl'
-	})
-	.state('suggestions',{
-		url: '/suggestions',
-		templateUrl: 'suggestions',
-		controller: 'HomeCtrl'
-	})
-	.state('more',{
-		url: '/more/:id',
-		templateUrl: 'more',
-		controller: 'MoreCtrl'
-	})
-})
-.controller('HomeCtrl', function($scope, $http){
-	$http.get('results').then(function(res){
-		$scope.clusters = res.data.artist;
-		$scope.songs = res.data.songs;
-	});
-})
-.controller('MoreCtrl', function($scope, $stateParams, $http){
-	$http.get('results').then(function(res){
-		var similars = res.data.artist[Number($stateParams.id)];
-		similars = similars.splice(1, similars.length);
-		$scope.similarArtists = similars;
-		var newSongs = res.data.songs[Number($stateParams.id)];
-		newSongs = newSongs.splice(1, newSongs.length);
-		$scope.songs = newSongs;
-	});
-})
-.controller('CheckCtrl', ['$scope', function($scope) {
-    $scope.choiceArray = [false,false,false,false,false,false,false,false,false,false];
-}])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/app");
+        $stateProvider
+            .state('app',{
+                url: '/app',
+                templateUrl: 'home',
+                controller: 'HomeCtrl'
+            })
+            .state('suggestions',{
+                url: '/suggestions',
+                templateUrl: 'suggestions',
+                controller: 'HomeCtrl'
+            })
+            .state('more',{
+                url: '/more/:id',
+                templateUrl: 'more',
+                controller: 'MoreCtrl'
+            })
+    })
+    .controller('HomeCtrl', function($scope, $http){
+
+        $scope.update = function (val, index, event) {
+            $(event.target).toggleClass("glyphicon-thumbs-up, glyphicon-thumbs-down");
+            $("#input_"+index).trigger('click');
+        }
+        $http.get('results').then(function(res){
+            $scope.clusters = res.data.artist;
+            $scope.songs = res.data.songs;
+        });
+    })
+    .controller('MoreCtrl', function($scope, $stateParams, $http){
+        $http.get('results').then(function(res){
+            var similars = res.data.artist[Number($stateParams.id)];
+            similars = similars.splice(1, similars.length);
+            $scope.similarArtists = similars;
+            var newSongs = res.data.songs[Number($stateParams.id)];
+            newSongs = newSongs.splice(1, newSongs.length);
+            $scope.songs = newSongs;
+        });
+    })
+    .controller('CheckCtrl', ['$scope', function($scope) {
+        $scope.choiceArray = [false,false,false,false,false,false,false,false,false,false];
+    }])
